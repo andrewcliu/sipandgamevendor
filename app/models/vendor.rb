2c8d5d948bd51b7d32cad7b138929d28d0bd8ad9
Andcliu <andcliu@gmail.com>
2026-08-18 19:07:30 -0700

    update

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,9 +1,18 @@
 class Vendor < ApplicationRecord
   belongs_to :event
-
+  before_validation :normalize_attributes
 
   validates :ig_handle, uniqueness: { 
     scope: :event_id, 
     message: "is already registered for this event" 
   }
+
+
+  private
+
+  def normalize_attributes
+    self.ig_handle = ig_handle&.strip&.downcase&.delete("@")
+    self.name      = name&.strip
+  end
+
 end
